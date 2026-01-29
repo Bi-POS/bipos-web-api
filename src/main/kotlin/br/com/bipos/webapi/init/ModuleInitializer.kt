34ede85adbf8edf67628a092f6785298df5d64fe
BiPOS <contato@bipos.com.br>
2026-01-29 13:17:34 -0300

    update module initializer (web-api)

--- src/main/kotlin/br/com/bipos/webapi/init/ModuleInitializer.kt
+++ src/main/kotlin/br/com/bipos/webapi/init/ModuleInitializer.kt
@@ -1,25 +1,28 @@
 package br.com.bipos.webapi.init
 
+import br.com.bipos.webapi.domain.module.Module
 import br.com.bipos.webapi.domain.module.ModuleType
 import br.com.bipos.webapi.module.ModuleRepository
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
-import br.com.bipos.webapi.domain.module.Module
 
 @Component
 class ModuleInitializer(
     private val moduleRepository: ModuleRepository
 ) {
-
     @EventListener(ApplicationReadyEvent::class)
     @Transactional
     fun init() {
-        ModuleType.entries.forEach { type ->
-            if (!moduleRepository.existsByName(type)) {
-                moduleRepository.save(Module(name = type))
+        try {
+            ModuleType.entries.forEach { type ->
+                if (!moduleRepository.existsByName(type)) {
+                    moduleRepository.save(Module(name = type))
+                }
             }
+        } catch (ex: Exception) {
+            println("⚠️ ModuleInitializer ignorado: ${ex.message}")
         }
     }
 }
