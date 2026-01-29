895029a7c8e6078b8f972e240fe097b2e348a184
BiPOS <contato@bipos.com.br>
2026-01-29 10:26:08 -0300

    remove bipos-domain dependency (standalone web-api)

--- src/main/kotlin/br/com/bipos/webapi/WebApiApplication.kt
+++ src/main/kotlin/br/com/bipos/webapi/WebApiApplication.kt
@@ -2,9 +2,7 @@ package br.com.bipos.webapi
 
 import br.com.bipos.webapi.security.auth.JwtProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.context.properties.EnableConfigurationProperties
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
