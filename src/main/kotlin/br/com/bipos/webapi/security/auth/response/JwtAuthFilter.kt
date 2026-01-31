d7c045a6a1ff6e45ee8899edbf2cbd92b616153e
BiPOS <contato@bipos.com.br>
2026-01-31 12:16:02 -0300

    insert shouldNotFilter in uploads  (pos-web)

--- src/main/kotlin/br/com/bipos/webapi/security/auth/response/JwtAuthFilter.kt
+++ src/main/kotlin/br/com/bipos/webapi/security/auth/response/JwtAuthFilter.kt
@@ -18,6 +18,11 @@ class JwtAuthFilter(
     private val userDetailsService: UserDetailsService
 ) : OncePerRequestFilter() {
 
+    override fun shouldNotFilter(request: HttpServletRequest): Boolean {
+        val path = request.servletPath
+        return path.startsWith("/uploads/")
+    }
+
     override fun doFilterInternal(
         request: HttpServletRequest,
         response: HttpServletResponse,
