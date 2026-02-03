d70f304766566744cf66a1bcd77cf3551e5c6f63
BiPOS <contato@bipos.com.br>
2026-02-03 16:38:52 -0300

    add rootcontroller

--- src/main/kotlin/br/com/bipos/webapi/security/RootController.kt
+++ src/main/kotlin/br/com/bipos/webapi/security/RootController.kt
@@ -1,5 +1,6 @@
 package br.com.bipos.webapi.security
 
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -9,4 +10,12 @@ class RootController {
     @GetMapping("/")
     fun ok(): Map<String, String> =
         mapOf("status" to "UP")
+}
+
+@RestController
+class ReadyController {
+
+    @GetMapping("/ready")
+    fun ready(): ResponseEntity<String> =
+        ResponseEntity.ok("OK")
 }
